21c4b6fe4cf6c109f5eb6f5336757f3ea47dbc5a
Yevhen <evgeniy.playid@icloud.com>
2024-05-18 11:21:23 +0300

    Add models & migrations file

--- parsing_site/parser/models.py
+++ parsing_site/parser/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Thread(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Content(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    url = models.URLField()
+    title = models.CharField(max_length=255)
+    availability = models.CharField(max_length=255)
+    producer = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+    is_download = models.BooleanField(default=False)
